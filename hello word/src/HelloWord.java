700e59dd79353277d392ab424a416b9ffca5778c
Muris <augustomurilomattos@gmail.com>
2025-04-11 13:07:45 -0300

    :tada: commit inicial

---
+++ hello word/src/HelloWord.java
@@ -0,0 +1,10 @@
+
+public class HelloWord {
+
+	public static void main(String[] args) {
+			System.out.println("Hello Word /n");
+			System.out.println("This is form from being my First Java Project /n"
+					+ "but it's going to be amusing");
+	}
+
+}
